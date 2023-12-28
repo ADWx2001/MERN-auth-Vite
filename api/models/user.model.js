62d7c887e399e0f6ea7360afb62276b39417e5cc
asanka119 <asankadilshanme@gmail.com>
2023-12-28 16:14:25 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+import { timeStamp } from "console";
+import { Mongoose } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+
+    }
+},{timeStamps:true});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
