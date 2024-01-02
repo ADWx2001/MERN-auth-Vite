c6d6f022cde7cc61528038e40fc09a4e28ce210a
asanka119 <asankadilshanme@gmail.com>
2024-01-02 15:45:31 +0530

    add contact form

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className="container">
+        <h1>Contact Me</h1>
+        <p>Feel free to reach out! I am always open to discussing new opportunities.</p>
+        <form action="#" method="post">
+            <label htmlFor="name">Name: </label>
+            <input type="text" name="name" id="name"/><br/>
+            <label htmlFor="email">Email: </label>
+            <input type="email" name="email" id="email"/><br />
+            <label htmlFor="message">Message: </label>
+            <textarea name="message" id="message" rows="5"></textarea><br />
+            <button type="submit">Submit</button>
+            
+        </form>
+    </div>
   )
 }
