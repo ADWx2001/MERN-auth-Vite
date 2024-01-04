f2231e8b1e3a74e403758c9ba49d51b6e4aea9a0
asanka119 <asankadilshanme@gmail.com>
2024-01-04 15:20:26 +0530

    fixed the redux signin.jsx error

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,58 +1,86 @@
-import { useState } from 'react';
-import {Link, useNavigate} from 'react-router-dom';
-import { signInFailure, signInStart, signInSuccess } from '../redux/user/userSlice';
-import { useDispatch, useSelector } from 'react-redux';
-
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Signin() {
-
   const [formData, setFormData] = useState({});
   // const [error, setError] = useState(false);
   // const [loading, setLoading] = useState(false);
-  const {loading, error} = useSelector((state=> state.user));  
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   // Function to handle form submission
-  const handleChange = (e)=>{
-    setFormData({...formData, [e.target.id]:e.target.value});
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
   // Function to handle form submission
-  const handleSubmit = async(e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
+
+    try {
       dispatch(signInStart());
-      const res = await fetch('/api/auth/signin', {method:'POST', headers:{'Content-Type':'application/json',}, body:JSON.stringify(formData)});
-      const data =await res.json();
-      
-      if(data.error){
-        dispatch(signInFailure(data.message));
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data));
         return;
       }
+      
+      
       dispatch(signInSuccess(data));
-      navigate('/');
-      }catch(error){
-        dispatch(signInFailure(error));
-      }
-    };
-  
+      navigate("/");
+
+    } catch (error) {
+      dispatch(signInFailure(error));
+    }
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7 ">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input type="email" placeholder="Email" id="email" className="bg-slate-100 p-3 rounded-lg" onChange={handleChange}/>
-        <input type="password" placeholder="Password" id="password" className="bg-slate-100 p-3 rounded-lg" onChange={handleChange}/>
-        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 ">{loading ? 'Loading...':'Sign In'}</button>
-      </form>  
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 "
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
 
       <div className="flex gap-2 mt-5">
         <p>Dont have an Account?</p>
-        <Link to='/sign-up'>
-          <span className='text-blue-500 '>Sign up</span>
+        <Link to="/sign-up">
+          <span className="text-blue-500 ">Sign up</span>
         </Link>
       </div>
-      <p className='text-red-600'>{error && 'Something went wrong!'}</p>
+      <p className="text-red-600">
+        {error ? error.message || "Something went wrong!" : ''}
+      </p>
     </div>
-  )
+  );
 }
